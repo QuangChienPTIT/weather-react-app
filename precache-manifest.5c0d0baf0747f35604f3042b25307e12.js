2df840166f5e8f3d5340dc0110fdb9059b14e147
QuangChienPTIT <pitpiglet2@gmail.com>
2020-10-16 11:18:18 +0700

    Updates

---
+++ precache-manifest.5c0d0baf0747f35604f3042b25307e12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f25f54073af7d698da07c3fa1f7d83e",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "696d85db09d6449612bf",
+    "url": "/weather-react-app/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "e7d77a6b3cd233ea8848",
+    "url": "/weather-react-app/static/css/main.b55099db.chunk.css"
+  },
+  {
+    "revision": "696d85db09d6449612bf",
+    "url": "/weather-react-app/static/js/2.5b073293.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-react-app/static/js/2.5b073293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d77a6b3cd233ea8848",
+    "url": "/weather-react-app/static/js/main.85ffa824.chunk.js"
+  },
+  {
+    "revision": "80a11080e1d5d2b66f46",
+    "url": "/weather-react-app/static/js/runtime-main.42a25d67.js"
+  },
+  {
+    "revision": "f52f8fb193620a7c3853737c69aef916",
+    "url": "/weather-react-app/static/media/WeatherOverlay.f52f8fb1.svg"
+  }
+]);
